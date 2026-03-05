f754e7711adc4f5510f41a4521f8d2cc272ffa4c
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-03-05 13:32:51 -0500

    edited and uploaded App.jsx
    added other src files

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import './App.css';
+import NewsGrid from './components/NewsGrid';
 
+
+function App() {
+  return (
+    <div className="App">
+      <NewsGrid />
+    </div>
+  );
+}
+
+export default App;
