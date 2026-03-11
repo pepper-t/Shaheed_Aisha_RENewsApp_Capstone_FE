5b1056751b7513c6105d57c056782b53cf40d8d4
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-03-10 23:04:13 -0400

    Refactor HomePage component structure
    Shortened page by removing two paragraphs.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,42 +31,8 @@ export default function HomePage() {
         repudiandae inventore. Molestiae voluptatem eius non doloremque deserunt
         enim necessitatibus incidunt natus, laudantium nihil. </p><br />
         
-        <p>Animi, tempora
-        vero aspernatur sequi, iste beatae velit cum provident explicabo quam
-        doloribus ipsam, vel inventore itaque non. Repudiandae cum inventore
-        deserunt architecto doloribus debitis quo odit ratione veritatis optio
-        consequatur, nisi nihil sint ea illum. Expedita corporis recusandae
-        velit nobis architecto rem in possimus nulla autem vero? Numquam, aut
-        hic officia ut inventore necessitatibus debitis cumque minima sunt
-        perferendis in, molestias saepe dolorem voluptatibus ullam consequatur
-        cupiditate ab velit a ducimus odio nostrum aspernatur aliquam
-        consequuntur. </p><br />
-        
-        <p>Eaque? Necessitatibus earum quasi vel, quidem sapiente
-        facere odit iusto consectetur doloribus hic deserunt accusantium amet
-        quaerat recusandae alias numquam tempora error nemo porro labore culpa
-        impedit deleniti at! Beatae, quo? Expedita porro excepturi provident
-        fugiat, iste nesciunt libero vel reprehenderit, saepe corporis
-        blanditiis architecto praesentium nobis totam corrupti nostrum nam
-        consequatur ipsam in pariatur temporibus nemo iure tenetur! Nemo,
-        nesciunt? Obcaecati aperiam earum rerum sint temporibus assumenda dicta
-        necessitatibus voluptatum laboriosam ipsum, voluptatibus nesciunt
-        tempora facere deserunt odio et debitis quia ratione velit iste atque.
-        Repudiandae recusandae dolore quidem corporis. Nisi non suscipit sunt
-        nostrum dignissimos vero. Temporibus numquam officiis quisquam aut
-        maxime officia ut cupiditate impedit perferendis, ratione suscipit cum?
-        Perferendis, cumque. Dolor delectus iusto placeat. Maiores, delectus
-        nostrum. Voluptates, ex. Perspiciatis deleniti sint minima magni ipsam
-        molestias, vero facere pariatur doloribus earum voluptatem, nesciunt
-        perferendis repudiandae cumque id nobis, suscipit minus. Iusto
-        reiciendis sit inventore maxime quaerat eveniet. Ab pariatur dicta
-        quaerat nisi fugiat nulla rerum error hic. Dolorum vero quia tempora,
-        ratione dolore odit! Aliquid modi distinctio ab impedit numquam. Placeat
-        aspernatur eveniet perferendis, at quibusdam labore? Velit autem a earum
-        accusamus esse voluptates error ipsa nobis cupiditate ipsum libero
-        numquam mollitia consectetur ratione, sint non vel repellendus! Odit
-        assumenda ut quae quisquam. Aliquam voluptatum doloribus corporis!
-      </p>
+       
     </div>
   );
 }
+
